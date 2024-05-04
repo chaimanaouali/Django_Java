8611f7d5dcccd07de12d28e69b3d92a7943dcbf6
chaimanaouali <chaima.naouali@esprit.tn>
2024-05-04 12:25:53 +0000

    Notification

--- src/main/java/com/example/assurance/Mail.java
+++ src/main/java/com/example/assurance/Mail.java
@@ -59,7 +59,7 @@ public class Mail {
             message.setSubject(subject); // Set the subject of the email
             message.setText(mmessage); // Set the content of the email
             message.setContent(
-                    "<h1 style =\"color:red\" >DON'T MISS YOUR APPOINTEMENT !! </h1> <br/> <img width=\"50%\" height=\"50%\" src=https://i.imgur.com/iYcBkOf.png>",
+                    "<h1 style =\"color:red\" >VOTRE REPONSE DEVIS EST BIEN PRETE!! </h1> <br/> ",
                     "text/html");
             // Enable debugging
             session.setDebug(true);
